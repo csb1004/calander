232f9c85c6ecd9032cb8298bf60c1db71be875ad
csb1004 <112150886+csb1004@users.noreply.github.com>
2023-08-13 09:46:19 +0000

    Update cli_db.py

--- cli_db.py
+++ cli_db.py
@@ -7,10 +7,10 @@ from database import *
 if __name__ == "__main__":
     match sys.argv[1]:
         case "push":
-            year = int(sys.argv[1])
-            month = int(sys.argv[2])
-            day = int(sys.argv[3])
-            event = json.loads(sys.argv[4])
+            year = int(sys.argv[2])
+            month = int(sys.argv[3])
+            day = int(sys.argv[4])
+            event = json.loads(sys.argv[5])
 
             db = CalendarDB(os.path.join(ROOT))
 
